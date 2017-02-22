c16c8ac7a9b97b7bda372ca7eee268c80996b72e
Nam Tran <namtran0110@gmail.com>
2017-02-22 15:32:25 +0700

    user can send email to each other

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,23 @@ class MessagesController < ApplicationController
   def index
     @messages = current_user.received_messages
   end
+
+  def new
+    @message = current_user.sent_messages.build
+  end
+
+  def create
+    @message = current_user.sent_messages.build message_params
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:sender_id, :recipient_id, :subject, :body)
+  end
 end
+
+
